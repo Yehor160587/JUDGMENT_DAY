f1a387639ea8db49e6d2a8051f0fb69f1f2cfe62
Anikina <lumootobor@gmail.com>
2022-06-17 14:57:10 +0300

    Scenario_#02_by_studentA

--- src/com/company/Conc.java
+++ src/com/company/Conc.java
@@ -1,13 +1,23 @@
 package com.company;
 
 public class Conc {
+
     public String concatStr (String a, String b) {
         return a + b;
     }
-    
+
+    static String s1 = "In 2022 I'll be the reach QA";
+    static int len1 = s1.length();
+
+    public static int strSize() {
+        return len1;
+    }
+
     public static void main (String[]args){
-Conc conc = new Conc();
-conc.concatStr("Привет", " GITHUB");
+        Conc conc = new Conc();
+        conc.concatStr("Привет", " GITHUB");
         System.out.println(conc.concatStr("Привет"," GITHUB"));
+
+        System.out.println(strSize());
     }
 }
